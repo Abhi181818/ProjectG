c41363b1be556382add79c80dac354dc3cc120a3
Abhishek Yadav <41482390+Abhi181818@users.noreply.github.com>
2024-12-19 04:13:24 +0000

    env variables

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,9 +3,18 @@ import react from '@vitejs/plugin-react-swc'
 import dotenv from 'dotenv'
 
 dotenv.config()
+
 export default defineConfig({
   plugins: [react()],
   define: {
-   
+    'process.env': {
+      VITE_FIREBASE_API_KEY: process.env.VITE_FIREBASE_API_KEY,
+      VITE_AUTH_DOMAIN: process.env.VITE_AUTH_DOMAIN,
+      VITE_PROJECT_ID: process.env.VITE_PROJECT_ID,
+      VITE_STORAGE_BUCKET: process.env.VITE_STORAGE_BUCKET,
+      VITE_MESSAGING_SENDER_ID: process.env.VITE_MESSAGING_SENDER_ID,
+      VITE_APP_ID: process.env.VITE_APP_ID,
+      VITE_MEASUREMENT_ID: process.env.VITE_MEASUREMENT_ID
+    }
   }
 })
